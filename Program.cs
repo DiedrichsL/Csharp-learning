0f6ad5a82dd9caa5d424dd10c013a9b4b03fcea2
DiedrichsL <diedrichs.l@outlook.com>
2026-01-07 14:51:21 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,12 @@
-﻿int Annee = 0;
-Console.WriteLine(" Quelle est votre année de naissance ? ");
-Annee = int.Parse(Console.ReadLine());
-int Age = 2026 - Annee;
-Console.WriteLine($"Vous êtes dans l'année de vos {Age} ans");
+﻿int X = 0;
+int y = 0;
+int pose = 0;
+
+Console.WriteLine("Entrez une valeur pour X et pour Y");
+X = int.Parse(Console.ReadLine());
+y = int.Parse(Console.ReadLine());
+Console.WriteLine($"Avant permutation: {X} et {y}");
+pose = X;
+X = y;
+y = pose;
+Console.WriteLine($"Après  permutation: {X} et {y}");
