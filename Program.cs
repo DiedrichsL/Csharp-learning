8f58469a230137902919405a1764032dac39979f
DiedrichsL <diedrichs.l@outlook.com>
2026-01-07 14:49:16 +0100

    Delete Program.cs

--- Program.cs
+++
@@ -1,6 +0,0 @@
-int Annee = 0;
-Console.WriteLine(" Quelle est votre année de naissance ? ");
-Annee = int.Parse(Console.ReadLine());
-int Age = 2026 - Annee;
-Console.WriteLine($"Vous êtes dans l'année de vos {Age} ans");
-
