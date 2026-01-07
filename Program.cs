847d0db367312ac14674fb86c135895349e9d524
DiedrichsL <diedrichs.l@outlook.com>
2026-01-07 14:14:45 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int X = 0;
+int y = 0;
+int pose = 0;
 
-int Annee = 0;
-Console.WriteLine(" Quelle est votre année de naissance ? ");
-Annee = int.Parse(Console.ReadLine());
-int Age = 2026 - Annee;
-Console.WriteLine($"Vous êtes dans l'année de vos {Age} ans");
+Console.WriteLine("Entrez une valeur pour X et pour Y");
+X = int.Parse(Console.ReadLine());
+y = int.Parse(Console.ReadLine());
+Console.WriteLine($"Avant permutation: {X} et {y}");
+pose = X;
+X = y;
+y = pose;
+Console.WriteLine($"Après  permutation: {X} et {y}");
