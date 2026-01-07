9ae6b6a0f671eb71ba0ad071faf4d77befe27beb
DiedrichsL <diedrichs.l@outlook.com>
2026-01-07 15:05:42 +0100

    Implement permutation program for two integers

---
+++ Exercice2_Permutation_Program.cs
@@ -0,0 +1,12 @@
+int X = 0;
+int y = 0;
+int pose = 0;
+
+Console.WriteLine("Entrez une valeur pour X et pour Y");
+X = int.Parse(Console.ReadLine());
+y = int.Parse(Console.ReadLine());
+Console.WriteLine($"Avant permutation: {X} et {y}");
+pose = X;
+X = y;
+y = pose;
+Console.WriteLine($"Après  permutation: {X} et {y}");
